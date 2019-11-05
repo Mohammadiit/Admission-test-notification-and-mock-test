1d1ca1bf85f970540a15a4d79e70eb365addc0d6
mohammad-ibrhaim <bsse0814@iit.du.ac.bd>
2019-11-05 21:01:32 +0600

    payment widget

--- src/app/admission-info/components/admission-info-list/admission-info-list.component.ts
+++ src/app/admission-info/components/admission-info-list/admission-info-list.component.ts
@@ -18,12 +18,42 @@ export class AdmissionInfoListComponent implements OnInit {
   ) { }
   items;
   ngOnInit() {
+    this.loadStripe();
     this.admissionInfoService.getAllAdmissionInfo()
       .subscribe(result => {
         this.items = result;
       });
   }
+  pay(amount) {
+    console.log('kkjjjjjjjjjjjjjjjjjjjjjjj');
+    var handler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_aeUUjYYcx4XNfKVW60pmHTtI',
+      locale: 'auto',
+      token: function (token: any) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+        console.log(token)
+        alert('Token Created!!');
+      }
+    });
 
+    handler.open({
+      name: 'Demo Site',
+      description: '2 widgets',
+      amount: amount * 100
+    });
+
+  }
+  loadStripe() {
+
+    if(!window.document.getElementById('stripe-script')) {
+      var s = window.document.createElement("script");
+      s.id = "stripe-script";
+      s.type = "text/javascript";
+      s.src = "https://checkout.stripe.com/checkout.js";
+      window.document.body.appendChild(s);
+    }
+  }
   goToDetails(item: any) {
     this.admissionInfoService.item = item;
     this.router.navigate([urlPaths.AdmissionInfo.AdmissionInfo.url]);
